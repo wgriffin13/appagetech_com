d057553134ffde62bab7dc43ed619aabd7ce6612
Preston Chaplin <preston.chaplin@gmail.com>
2019-09-05 10:27:13 -0400

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,125 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
+import { EquirectangularToCubeGenerator } from "three/examples/jsm/loaders/EquirectangularToCubeGenerator.js";
+import { PMREMGenerator } from "three/examples/jsm/pmrem/PMREMGenerator.js";
+import { PMREMCubeUVPacker } from "three/examples/jsm/pmrem/PMREMCubeUVPacker.js";
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let cubeGenerator, renderer, scene;
+
+class App extends Component {
+  componentDidMount() {
+    this.sceneSetup();
+    this.animate();
+    this.loadAssets();
+  }
+
+  sceneSetup = () => {
+    this.container = document.createElement("div");
+    document.body.appendChild(this.container);
+    this.camera = new THREE.PerspectiveCamera(
+      35,
+      window.innerWidth / window.innerHeight,
+      0.25,
+      20
+    );
+    this.camera.position.z = 7;
+    scene = new THREE.Scene();
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.gammaOutput = true;
+    this.container.appendChild(renderer.domElement);
+    window.addEventListener("resize", this.onWindowResize, false);
+  };
+
+  loadAssets = () => {
+    new RGBELoader()
+      .setDataType(THREE.UnsignedByteType)
+      .setPath("textures/")
+      .load("hdrvfx_chanc_1_n1_v3_3k_Env.hdr", function(texture) {
+        cubeGenerator = new EquirectangularToCubeGenerator(texture, {
+          resolution: 5000
+        });
+        cubeGenerator.update(renderer);
+        const pmremGenerator = new PMREMGenerator(
+          cubeGenerator.renderTarget.texture
+        );
+        pmremGenerator.update(renderer);
+        const pmremCubeUVPacker = new PMREMCubeUVPacker(
+          pmremGenerator.cubeLods
+        );
+        pmremCubeUVPacker.update(renderer);
+        const envMap = pmremCubeUVPacker.CubeUVRenderTarget.texture;
+
+        // Models
+        const Type = new GLTFLoader().setPath("/models/");
+        Type.load("contact_type.glb", function(gltf) {
+          gltf.scene.traverse(function(child) {
+            if (child.isMesh) {
+              child.material = new THREE.MeshStandardMaterial({
+                envMap: envMap,
+                color: 0x000000,
+                metalness: 1,
+                roughness: 0.2
+              });
+            }
+          });
+          scene.add(gltf.scene);
+        });
+
+        const Icon = new GLTFLoader().setPath("/models/");
+        Icon.load("contact_Icon.glb", function(gltf) {
+          gltf.scene.traverse(function(child) {
+            if (child.isMesh) {
+              child.material = new THREE.MeshStandardMaterial({
+                envMap: envMap,
+                envMapIntensity: 1.5,
+                emissive: 0xfff000,
+                emissiveIntensity: 0.2,
+                color: 0xfddf73,
+                metalness: 1,
+                roughness: 0.2
+              });
+            }
+          });
+          scene.add(gltf.scene);
+        });
+        pmremGenerator.dispose();
+        pmremCubeUVPacker.dispose();
+
+        // scene.background = cubeGenerator.renderTarget;
+        scene.background = new THREE.Color(0x000000);
+      });
+  };
+
+  animate = () => {
+    requestAnimationFrame(this.animate);
+    if (this.Icon && this.Type) {
+      this.Icon.rotation.y += 0.005;
+      this.Type.rotation.y += 0.005;
+    }
+    renderer.render(scene, this.camera);
+  };
+
+  handleWindowResize = () => {
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+  };
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleWindowResize);
+    window.cancelAnimationFrame(this.requestID);
+    this.controls.dispose();
+  }
+
+  render() {
+    return <div ref={el => (this.mount = el)} />;
+  }
 }
 
 export default App;
