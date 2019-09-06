21252748ba7e438b4cf784a56f3c4b759ccb1a84
Preston Chaplin <preston.chaplin@gmail.com>
2019-09-06 16:17:53 -0400

    removed lights, IBL lighting only

--- src/Landing.js
+++ src/Landing.js
@@ -5,7 +5,6 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
 import { EquirectangularToCubeGenerator } from "three/examples/jsm/loaders/EquirectangularToCubeGenerator.js";
 import { PMREMGenerator } from "three/examples/jsm/pmrem/PMREMGenerator.js";
-import { RectAreaLightUniformsLib } from "three/examples/jsm/lights/RectAreaLightUniformsLib.js";
 import { PMREMCubeUVPacker } from "three/examples/jsm/pmrem/PMREMCubeUVPacker.js";
 
 let cubeGenerator, renderer, scene, type, icon;
@@ -14,7 +13,6 @@ class Landing extends Component {
   componentDidMount() {
     this.sceneSetup();
     this.animate();
-    this.lighting();
   }
 
   sceneSetup = () => {
@@ -33,7 +31,7 @@ class Landing extends Component {
     new RGBELoader()
       .setDataType(THREE.UnsignedByteType)
       .setPath("textures/")
-      .load("diyHdri_01k.hdr", function(texture) {
+      .load("diyHdri_01o.hdr", function(texture) {
         cubeGenerator = new EquirectangularToCubeGenerator(texture, {
           resolution: 5000
         });
@@ -54,7 +52,7 @@ class Landing extends Component {
         // Models
         new GLTFLoader()
           .setPath("/models/")
-          .load("Logo_Type_Large.glb", function(gltf) {
+          .load("Logo_Type_Large_2.glb", function(gltf) {
             gltf.scene.traverse(function(child) {
               if (child.isMesh) {
                 type = child;
@@ -80,8 +78,8 @@ class Landing extends Component {
                 child.material = new THREE.MeshStandardMaterial({
                   envMap: envMap,
                   envMapIntensity: 0.9,
-                  emissive: 0x9af8fb,
-                  emissiveIntensity: 0.1,
+                  // emissive: 0x9af8fb,
+                  // emissiveIntensity: 0.1,
                   color: 0xfddf73,
                   metalness: 1,
                   roughness: 0.1
@@ -106,49 +104,6 @@ class Landing extends Component {
     this.controls.target.set(0, 0, 0);
     this.controls.update();
     window.addEventListener("resize", this.onWindowResize, false);
-  };
-
-  lighting = () => {
-    // Blue back
-    RectAreaLightUniformsLib.init();
-    this.rectLight1 = new THREE.RectAreaLight(0x0030ff, 1, 5, 50);
-    this.rectLight1.position.set(0, 0, -10);
-    this.rectLight1.lookAt(0, 0, 0);
-    scene.add(this.rectLight1);
-
-    // Blue 2
-    this.rectLight2 = new THREE.RectAreaLight(0x0030ff, 1, 5, 50);
-    this.rectLight2.position.x = -9;
-    this.rectLight2.position.z = 3;
-    this.rectLight2.lookAt(0, 0, 0);
-    scene.add(this.rectLight2);
-
-    // Red
-    this.rectLight3 = new THREE.RectAreaLight(0xfb3f3f, 1, 5, 50);
-    this.rectLight3.position.x = 8;
-    this.rectLight3.position.z = 3;
-    this.rectLight3.lookAt(0, 0, 0);
-    scene.add(this.rectLight3);
-
-    // Green
-    this.rectLight4 = new THREE.RectAreaLight(0x18ff00, 1, 5, 50);
-    this.rectLight4.position.x = -8;
-    this.rectLight4.position.z = -6;
-    this.rectLight4.lookAt(0, 0, 0);
-    scene.add(this.rectLight4);
-
-    //Green 2
-    this.rectLight5 = new THREE.RectAreaLight(0x18ff00, 1, 10, 50);
-    this.rectLight5.position.set(0, 0, 10);
-    this.rectLight5.lookAt(0, 0, 0);
-    scene.add(this.rectLight5);
-
-    // Red 2
-    this.rectLight6 = new THREE.RectAreaLight(0xfb3f3f, 1, 15, 50);
-    this.rectLight6.position.x = -10;
-    this.rectLight6.position.z = 10;
-    this.rectLight6.lookAt(0, 0, 0);
-    scene.add(this.rectLight6);
   };
 
   onWindowResize = () => {
