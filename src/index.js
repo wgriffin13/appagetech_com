f2eb4665a214c074d3ba17868c3782dc0db78f45
William Griffin <w.griffin13@gmail.com>
2019-09-10 14:14:47 -0700

    working transition example

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import "./index.css";
 // import App from './App';
 import Home from "./Home";
 // import Landing from "./Landing";
+import TransitionExample from './TransitionExample';
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Home />, document.getElementById("root"));
+ReactDOM.render(<TransitionExample />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
