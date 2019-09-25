de7fa3f12152e1ed5e6e8e25a891b87713946a5e
Preston Chaplin <preston.chaplin@gmail.com>
2019-09-25 09:37:02 -0400

    switched to a new fork

--- src/2D/Contact.js
+++ src/2D/Contact.js
@@ -1,25 +1,37 @@
 import React, { Component } from "react";
 import Container from "react-bootstrap/Container";
+import Jumbotron from "react-bootstrap/Jumbotron";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import "./contact.css";
 
 class Contact extends Component {
   render() {
-    return(
-    <Container>
-        <Row>
-          <Col>
-            <h4>William Griffin</h4>
-            <i>Los Angeles, California</i>
-            <a href={`mailto: william@appagetech.com`}>william@appagetech.com</a>
-            <a href={`tel:949-632-3021`}>(949)632-3021 </a>
-          </Col>
-          <Col>
-            <h4>Preston Chaplin</h4>
-          </Col>
-        </Row>
+    return (
+      <Container className="container">
+        {/* <Jumbotron> */}
+        <div className="col">
+          <h4>William Griffin</h4>
+          <i>Los Angeles, California</i>
+          <br />
+          <a href={`mailto: william@appagetech.com`}>william@appagetech.com</a>
+          <br />
+          <a href={`tel:949-632-3021`}>(949)632-3021 </a>
+        </div>
+        <Col>
+          <h4>Preston Chaplin</h4>
+
+          <i>New York, New York</i>
+          <br />
+
+          <a href={`mailto: preston@appagetech.com`}>preston@appagetech.com</a>
+          <br />
+
+          <a href={`tel:646-271-3127`}>(646)271-3127 </a>
+        </Col>
+        {/* </Jumbotron> */}
       </Container>
-    )
+    );
   }
 }
 
