20f93a23902193da4b7ccc89d31900dd76d07439
Preston Chaplin <preston.chaplin@gmail.com>
2019-10-01 09:58:34 -0400

    mergeflicts

--- src/LandingTransition.js
+++ src/LandingTransition.js
@@ -5,11 +5,11 @@ import { EquirectangularToCubeGenerator } from "three/examples/jsm/loaders/Equir
 import { PMREMGenerator } from "three/examples/jsm/pmrem/PMREMGenerator.js";
 import { PMREMCubeUVPacker } from "three/examples/jsm/pmrem/PMREMCubeUVPacker.js";
 
-export default function LandingTransition(renderer, clearColor, toggleTransitionFunc) {
+export default function LandingTransition(renderer, toggleTransitionFunc) {
     // Initial variables
     const raycaster = new THREE.Raycaster();
     const mouse = new THREE.Vector2();
-    //this.clearColor = clearColor;
+
     // Scene & Camera
     this.camera = new THREE.PerspectiveCamera(
       40,
@@ -57,7 +57,6 @@ export default function LandingTransition(renderer, clearColor, toggleTransition
                 gltf.scene.traverse(function(child) {
                     if (child.isMesh) {
                     type = child;
-                    //console.log("type ", type);
                     child.material = new THREE.MeshStandardMaterial({
                         envMap: envMap,
                         envMapIntensity: 2,
@@ -80,8 +79,6 @@ export default function LandingTransition(renderer, clearColor, toggleTransition
                     child.material = new THREE.MeshStandardMaterial({
                         envMap: envMap,
                         envMapIntensity: 0.9,
-                        // emissive: 0x9af8fb,
-                        // emissiveIntensity: 0.1,
                         color: 0xfddf73,
                         metalness: 1,
                         roughness: 0.1
@@ -94,7 +91,6 @@ export default function LandingTransition(renderer, clearColor, toggleTransition
         pmremGenerator.dispose();
         pmremCubeUVPacker.dispose();
 
-        // scene.background = cubeGenerator.renderTarget;
         scene.background = new THREE.Color(0x000000);
     });
 
@@ -125,24 +121,24 @@ export default function LandingTransition(renderer, clearColor, toggleTransition
     const starForge = () => {
         var starQty = 800000;
         const starGeometry = new THREE.SphereGeometry(200, 10, 10);
-    
+
         const materialOptions = {
           size: 0.06, //I know this is the default, it's for you.  Play with it if you want.
           transparency: true,
           opacity: 0.7
         };
-    
+
         const starStuff = new THREE.PointCloudMaterial(materialOptions);
-    
+
         for (var i = 0; i < starQty; i++) {
           var starVertex = new THREE.Vector3();
           starVertex.x = Math.random() * 200 - 100;
           starVertex.y = Math.random() * 200 - 100;
           starVertex.z = Math.random() * 100 - 100;
-    
+
           starGeometry.vertices.push(starVertex);
         }
-    
+
         const stars = new THREE.PointCloud(starGeometry, starStuff);
         scene.add(stars);
     };
