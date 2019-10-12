9a7dc1f35ca4f679526d181348f8335378f23cf0
Preston Chaplin <preston.chaplin@gmail.com>
2019-10-11 21:02:48 -0400

    added restricted orbital controls

--- src/Home.js
+++ src/Home.js
@@ -188,7 +188,7 @@ class Home extends Component {
     // Typical usage (don't forget to compare props):
     if (this.props !== prevProps) {
       if (this.props.location.pathname !== `/${this.state.location}`) {
-        this.matchRenderToLocation()
+        this.matchRenderToLocation();
       }
     }
   }
@@ -252,7 +252,7 @@ class Home extends Component {
 
   toggleTransition = () => {
     if (this.state.landingPage === true) {
-      this.setState({ landingPage: false, location: 'home' });
+      this.setState({ landingPage: false, location: "home" });
       this.props.history.push("/home");
     } else {
       this.setState({ landingPage: true });
@@ -263,20 +263,20 @@ class Home extends Component {
     camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.25, 4000);
     camera.position.set(0, 0, 224);
     camera.lookAt(0, 0, 0);
-   
+
     glRenderer = this.createGlRenderer();
     controls = new OrbitControls(camera, glRenderer.domElement);
     controls.enableZoom = false;
-   
-    // How far you can orbit vertically, upper and lower limits.
-// Range is 0 to Math.PI radians.
-controls.minPolarAngle = 0; // radians
-controls.maxPolarAngle = 0; // radians
 
-// How far you can orbit horizontally, upper and lower limits.
-// If set, must be a sub-interval of the interval [ - Math.PI, Math.PI ].
-controls.minAzimuthAngle = - Math.PI / 2 ; // radians
-controls.maxAzimuthAngle = Math.PI / 2; // radians
+    // How far you can orbit vertically, upper and lower limits.
+    // Range is 0 to Math.PI radians.
+    controls.minPolarAngle = 0; // radians
+    controls.maxPolarAngle = 0; // radians
+
+    // How far you can orbit horizontally, upper and lower limits.
+    // If set, must be a sub-interval of the interval [ - Math.PI, Math.PI ].
+    controls.minAzimuthAngle = -Math.PI / 2; // radians
+    controls.maxAzimuthAngle = Math.PI / 2; // radians
 
     cssRenderer = this.createCssRenderer();
     container = document.createElement("div");
@@ -320,13 +320,13 @@ controls.maxAzimuthAngle = Math.PI / 2; // radians
         cssComponentDisplayed: "",
         show2D: false,
         showWater: true,
-        location: 'home'
+        location: "home"
       });
       Object.entries(reactComponentsObj).forEach(([key, value]) => (value.position.z = offScreenZPosition2D));
       // Pushes location back to home
       if (this.props.location.pathname !== `/home`) {
         this.props.history.push(`/home`);
-      };
+      }
       // Toggle Icons ON
       glScene.traverse(function(child) {
         if (
@@ -1057,10 +1057,10 @@ controls.maxAzimuthAngle = Math.PI / 2; // radians
       glRenderer.render(glScene, camera);
       cssRenderer.render(cssScene, camera);
 
-      if ( !this.state.show2D){
-      camera.position.x += ( mouseCoords.x - camera.position.x ) * .05;
-      camera.position.y += ( - mouseCoords.y - camera.position.y ) * .05;
-      camera.lookAt( glScene.position );
+      if (!this.state.show2D) {
+        camera.position.x += (mouseCoords.x - camera.position.x) * 0.05;
+        camera.position.y += (-mouseCoords.y - camera.position.y) * 0.05;
+        camera.lookAt(glScene.position);
       }
 
       if (this.state.reactComponentsMounted === false) {
