5da488ab82ab3dd3f6b9f82ba07ad9d6acabc168
William Griffin <w.griffin13@gmail.com>
2019-10-24 08:39:51 -0700

    changed app default port

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const app = require('./app')
 const { sync } = require('./db/sync');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const host = process.env.HOST || '0.0.0.0';
 
 if (process.env.NODE_ENV === 'development') {
